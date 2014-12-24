bbee1464a68c11d193822c05f1b6906889342311
shiguodong <oreatial@gmail.com>
2014-12-24 18:00:42 +0800

    ddd

---
+++ config/unicorn.rb
@@ -0,0 +1,10 @@
+root = "/home/www/apps/jesscia"
+working_directory root
+pid "#{root}/tmp/pids/unicorn_jessica.pid"
+stderr_path "#{root}/log/unicorn_jessica.stderr.log"
+stdout_path "#{root}/log/unicorn_jessica.log"
+
+listen "/tmp/unicorn_jessica.sock"
+worker_processes 3
+timeout 30
+#preload_app true
